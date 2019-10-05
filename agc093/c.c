bfdb30e2e56eadb179d92c4bf676605e37781a22
anderzheng <zhengqihua@163.com>
2019-10-06 00:03:26 +0800

    Create c.c

---
+++ agc093/c.c
@@ -0,0 +1,7 @@
+/* C - Division by Two with Something */
+
+/* binary bit move operation:
+   if remove 1 bit at right, then add 1 bit at left(with the oppsite value)
+   so, normal integers need 2*N times to get the orignal value(N is the bit nums).
+   and the symmetric?? bits need only N/2 times
+*/
